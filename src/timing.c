b18e4f07531ed59e6e47aa581a2f7aad246693a1
Scott Levy <sllevy@sandia.gov>
2020-07-20 17:22:19 -0600

    Add debug code.

--- src/timing.c
+++ src/timing.c
@@ -29,6 +29,10 @@ get_ticks_per_second( int rounds )
                 t2 = rdtsc();
                 end = PMPI_Wtime();
 
+                // !!!! DEBUG !!!!
+                printf("(get_ticks_per_second) TIMESTAMPS: t1 = %ld / t2 = %ld / t2-t1 = %ld / start = %f / end = %f\n", 
+                       t1, t2, t2-t1, start, end); 
+
                 res[ i ] = ( uint64_t )( ceil( ( ( double )t2 - ( double )t1 ) /
                                 ( end - start ) ) );
         }
